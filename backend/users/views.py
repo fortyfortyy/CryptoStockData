b4e9333e497a0d460a4e6d1c0d0ca1abda321551
fortyfortyy <d.pacek1@gmail.com>
2022-11-09 22:33:49 +0100

    create ActivateAccountView

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,13 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
 
+from django.http import HttpResponseRedirect
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import smart_str
+
+from users.utils import account_token
 from users.models import UserProfile
 from users.serializers import RegisterSerializer
 
@@ -24,3 +31,35 @@ class RegisterUserView(generics.CreateAPIView):
 
 
 register_user_view = RegisterUserView.as_view()
+
+
+class ActivateAccountView(APIView):
+    """
+    Checks whether the link is valid for the given user.
+    Then activate account.
+    """
+    model = UserProfile
+    renderer_classes = [JSONRenderer]
+
+    # def get(self, request, *args, **kwargs):
+    #     return HttpResponseRedirect(f"/#/account/activate/{kwargs['uidb64']}/{kwargs['token']}/")
+
+    def post(self, request, *args, **kwargs):
+        try:
+            uid = smart_str(urlsafe_base64_decode(kwargs.get('uidb64', None)))
+            profile = self.model.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, KeyError, self.model.DoesNotExist):
+            profile = None
+
+        if profile is not None and account_token.check_token(profile, kwargs.get('token', None)):
+            profile.is_active = True
+            profile.save()
+
+            return Response({'account': 'Account has been activated!'}, status=status.HTTP_200_OK,
+                            content_type='application/javascript')
+
+        content = {'account': 'Link is invalid or account is already activated'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST, content_type='application/javascript')
+
+
+activate_account_view = ActivateAccountView.as_view()
