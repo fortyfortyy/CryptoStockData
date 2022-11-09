40d7fc052d819bfebb70122dfcc342862e94b2a2
fortyfortyy <d.pacek1@gmail.com>
2022-11-09 22:47:57 +0100

    create custom token obtain pair serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,8 +1,13 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
+from rest_framework_simplejwt.serializers import TokenObtainSerializer
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.settings import api_settings
 
 from django.core import exceptions
 from django.core.validators import validate_email
+from django.contrib.auth.models import update_last_login
 from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.models import UserProfile
 
@@ -88,4 +93,37 @@ class ChangePasswordSerializer(serializers.Serializer):
             raise serializers.ValidationError({'password2': _("Passwords are not the same.")})
         elif len(data['password']) > 128 or len(data['password2']) > 128:
             raise serializers.ValidationError("Passwords are too long")
+        return data
+
+
+class InActiveUser(AuthenticationFailed):
+    status_code = status.HTTP_406_NOT_ACCEPTABLE
+    default_detail = "User is not active, please confirm your email"
+    default_code = 'user_is_inactive'
+
+
+class CustomTokenObtainPairSerializer(TokenObtainSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = RefreshToken.for_user(user)
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        if not self.user.is_active:
+            raise InActiveUser()
+
+        refresh = self.get_token(self.user)
+
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
         return data
