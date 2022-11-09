26dd7546d1c4a96645592947f4dac4cfc7ce7ef9
fortyfortyy <d.pacek1@gmail.com>
2022-11-09 22:42:04 +0100

    create function to send link password to given email

--- backend/users/utils.py
+++ backend/users/utils.py
@@ -1,8 +1,29 @@
+import threading
+
+#  imports needed for email
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.conf import settings
+from django.contrib.sites.models import Site
+
 from six import text_type
+from templated_email import get_templated_mail
+
 # generate token
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 # ---
 from users.models import UserProfile
+
+from stockapp import settings
+
+
+class EmailThread(threading.Thread):
+    def __init__(self, email_message):
+        self.email_message = email_message
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.email_message.send()
 
 
 class TokenGenerator(PasswordResetTokenGenerator):
@@ -14,3 +35,25 @@ class TokenGenerator(PasswordResetTokenGenerator):
 
 
 account_token = TokenGenerator()
+
+
+def send_reset_password_email(profile: UserProfile) -> None:
+    current_domain = Site.objects.get_current().domain
+    email_template = get_templated_mail(
+        template_name='password_reset_email.html',
+        from_email=settings.EMAIL_HOST_USER,
+        to=[profile.email],
+        context={
+            'profile': profile,
+            'domain': current_domain,
+            'uid': urlsafe_base64_encode(force_bytes(profile.id)),
+            'token': account_token.make_token(profile),
+        },
+        template_prefix="emails/",
+        template_suffix="html",
+    )
+    send_html_mail(email_template)
+
+
+def send_html_mail(templated_email_to_send) -> None:
+    EmailThread(templated_email_to_send).start()
