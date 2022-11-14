7a74d2523376288ff8221b6b8394c498fe0fb327
fortyfortyy <d.pacek1@gmail.com>
2022-11-14 02:40:15 +0100

    apply headers into request

--- backend/stocks/utils.py
+++ backend/stocks/utils.py
@@ -2,8 +2,8 @@ import requests
 import json
 
 
-def fetch_data_from_url(url):
-    response = requests.get(url)
+def fetch_data_from_url(url, headers):
+    response = requests.get(url, headers=headers)
     return response.text if response.status_code == 200 else None
 
 
